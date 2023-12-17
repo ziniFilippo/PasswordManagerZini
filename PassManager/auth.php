a2087c0cca153037d077a23f5f471683c09f7cd7
ziniFilippo <101709141+ziniFilippo@users.noreply.github.com>
2023-12-17 20:40:13 +0000

    update

--- PassManager/auth.php
+++ PassManager/auth.php
@@ -9,15 +9,15 @@
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $username = $_POST['username'];
         $password = $_POST['password'];
-
         $stmt = $conn->prepare("SELECT * FROM ACCOUNT WHERE USERNAME = ?");
         $stmt->bind_param("s", $username);
         $stmt->execute();
         $result = $stmt->get_result();
-
         if ($result->num_rows > 0) {
             $user = $result->fetch_assoc();
-            if (password_verify($password, $user['MD5'])) {
+            $salt = $user['salt'];
+            $password = $password.$salt;
+            if (password_verify($password, $user['sha3'])) {
                 session_start();
                 $_SESSION['username'] = $username;
                 redirect("./home.php");
--- PassManager/register_control.php
+++ PassManager/register_control.php
@@ -9,9 +9,11 @@
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $username = $_POST['username'];
         $password = $_POST['password'];
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-        $stmt = $conn->prepare("INSERT INTO ACCOUNT (username, MD5) VALUES (?, ?)");
-        $stmt->bind_param("ss", $username, $hashed_password);
+        $salt = random_bytes($length);
+        $password = $password.$salt;
+        $hashed_password = hash("sha3",$password);
+        $stmt = $conn->prepare("INSERT INTO ACCOUNT (mail, sha3 , salt) VALUES (?, ?, ?)");
+        $stmt->bind_param("ss", $username, $hashed_password, $salt);
         $stmt->execute();
 
         session_start();
